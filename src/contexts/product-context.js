2e022eb5d50adbaf5b2cadf0b55ed132f66d78a9
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-05-03 03:56:51 +0530

    implemented 'include out of stock' functionality

--- src/contexts/product-context.js
+++ src/contexts/product-context.js
@@ -31,26 +31,31 @@ export const ProductProvider = ({ children }) => {
   const searchFilteredProducts =
     allFilters?.search?.length > 0
       ? data?.filter(({ name }) =>
-          name.toLowerCase().includes(allFilters?.search.toLowerCase())
+          name?.toLowerCase()?.includes(allFilters?.search?.toLowerCase())
         )
       : data;
 
   const radioFilteredProducts = allFilters?.sort
-    ? searchFilteredProducts.sort((prod1, prod2) =>
-        allFilters.sort === "HTL"
+    ? searchFilteredProducts?.sort((prod1, prod2) =>
+        allFilters?.sort === "HTL"
           ? prod2.price - prod1.price
           : prod1.price - prod2.price
       )
     : searchFilteredProducts;
 
-  const checkboxFilteredProducts =
-    allFilters?.filters.length > 0
-      ? radioFilteredProducts.filter((product) =>
-          allFilters.filters.every((filter) =>
-            filter === "inStock" ? !product[filter] : product[filter]
-          )
-        )
-      : radioFilteredProducts;
+  const checkboxFilteredProducts = () => {
+    let filtersWithoutInStock = allFilters?.filters?.filter(
+      (filter) => filter !== "includeOutOfStock"
+    );
+    const checkboxFilteredData = radioFilteredProducts?.filter((product) =>
+      filtersWithoutInStock?.every((filter) => product[filter])
+    );
+    if (allFilters?.filters?.includes("includeOutOfStock")) {
+      return checkboxFilteredData;
+    } else {
+      return checkboxFilteredData?.filter(({ inStock }) => inStock);
+    }
+  };
 
   return (
     <ProductContext.Provider
